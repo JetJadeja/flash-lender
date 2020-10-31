bcc7ad75711358277e75634ab97e203e41be2a58
Jet <jetjadeja@gmail.com>
2020-10-31 11:05:11 -0700

    Removed the need for a supplier

--- contracts/Lender.sol
+++ contracts/Lender.sol
@@ -3,31 +3,46 @@ pragma solidity ^0.5.0;
 
 import "@openzeppelin/contracts/ownership/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-
-import "@openzeppelin/contracts/token/erc20/ERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
+import "@openzeppelin/contracts/token/erc20/IERC20.sol";
+import "@openzeppelin/contracts/token/erc20/SafeERC20.sol";
 import "./interfaces/IBorrower.sol";
 
 
 /**
     @title Flash Lender
     @author Jet Jadeja <jet.j.developing@gmail.com>
-    @notice This contract uses the funds provided by the supplier to execute Flash Loans and ensure the money is returned
+    @notice This is the main contract behind the Flash Loan system. It lends money to the borrower and ensures it is paid back.
 */
 contract Lender is Ownable, ReentrancyGuard{
     using SafeMath for uint256;
+    using SafeERC20 for IERC20;
 
     ///@dev An unsigned integer that represents the loan fee
     uint256 public fee;
+
+    ///@dev Stores the amount and token owed
+    struct Owed {
+        address token;
+        uint256 amount;
+    }
+
+    ///@dev The amount of money that each user owes
+    mapping(address => Owed) internal principal;
 
     /**
     @dev Confirms that the Supplier has sufficient funds and enforce the token is payed back (with the calculated fee)
     @param token The ERC20 Token Address
     @param amount The amount being borrowed
     */
-    modifier isReturned(address token, uint256 amount) {
+    modifier isReturned(address token, address borrower, uint256 amount) {
+
+        require(balanceOf(token)/2 > amount); // Cannot borrow more than half of supply
+        principal[borrower] = Owed(token, amount);
         _;
+        require(principal[borrower].amount == 0);
+
     }
 
     constructor(uint256 _fee) public {
@@ -42,13 +57,32 @@ contract Lender is Ownable, ReentrancyGuard{
         fee = _fee;
     }
 
+    function balanceOf(address token) public returns(uint256) {
+        return IERC20(token).balanceOf(address(this));
+    }
+
     /**
-    @dev Borrow money from the supplier and lend it to the Borrower
+    @dev Lend money to the Borrower
     @param token The ERC20 Token Address
     @param amount The amount being borrowed
     */
-    function loan(address token, uint256 amount) external isReturned(token, amount) returns(bool) {
+    function loan(address token, uint256 amount) external isReturned(token, msg.sender, amount) returns(bool) { 
+        IERC20(token).transfer(msg.sender, amount);
         return IBorrower(msg.sender).borrow(token, amount);
+    }
+
+    /**
+    @dev Repay tokens lended to the borrower
+    */
+    function repay() external{
+        IERC20(principal[msg.sender].token).safeTransferFrom
+        (
+            msg.sender, 
+            address(this), 
+            principal[msg.sender].amount
+        );
+
+        delete principal[msg.sender];
     }
 
 }
