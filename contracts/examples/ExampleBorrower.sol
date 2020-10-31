bc0184e915e1b74d3ec6cf7ed23eb16667c54cf4
Jet <jetjadeja@gmail.com>
2020-10-31 11:15:13 -0700

    Account for File and Contract Name Changes

--- contracts/examples/ExampleBorrower.sol
+++ contracts/examples/ExampleBorrower.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.5.0;
 
-import "../interfaces/Borrower.sol";
+import "../interfaces/IBorrower.sol";
 import "@openzeppelin/contracts/token/erc20/ERC20.sol";
 
-contract ExampleBorrower is Borrower {
+contract ExampleBorrower is IBorrower {
     function borrow(address token, address supplier, uint256 amount) external returns (bool) {
         return ERC20(token).transfer(supplier, amount);
     }
