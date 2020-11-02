2e942a752b8fca6f0a86c57c9ad92be1018f89a3
Jet <jetjadeja@gmail.com>
2020-11-02 10:01:18 -0800

    Borrower now calls the Lender.repay function

--- contracts/examples/ExampleBorrower.sol
+++ contracts/examples/ExampleBorrower.sol
@@ -2,9 +2,11 @@ pragma solidity ^0.5.0;
 
 import "../interfaces/IBorrower.sol";
 import "@openzeppelin/contracts/token/erc20/ERC20.sol";
+import "../Lender.sol";
 
 contract ExampleBorrower is IBorrower {
-    function borrow(address token, address supplier, uint256 amount) external returns (bool) {
-        return ERC20(token).transfer(supplier, amount);
+    function borrow(address token, uint256 amount) external returns (bool) {
+        Lender(msg.sender).repayTokens();
+        return true;
     }
 }
