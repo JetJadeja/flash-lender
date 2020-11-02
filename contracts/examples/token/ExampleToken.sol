22c241f20f8ffb004747a11fa71b2c4b5d4462c7
Jet <jetjadeja@gmail.com>
2020-11-02 10:00:25 -0800

    Add Lender Allowance Increase in Token Constructor

--- contracts/examples/token/ExampleToken.sol
+++ contracts/examples/token/ExampleToken.sol
@@ -4,7 +4,8 @@ import "@openzeppelin/contracts/token/erc20/ERC20.sol";
 
 //An Example Token used for Testing
 contract ExampleToken is ERC20 {
-    constructor(address bank) ERC20() public {
-        _mint(bank, 10000);
+    constructor(address lender) ERC20() public {
+        _mint(lender, 10000);
+        increaseAllowance(lender, 100);
     }
 }
--- test/test.js
+++
@@ -1,4 +0,0 @@
-import { tsGenerator } from 'ts-generator'
-import { TypeChain } from 'typechain/dist/TypeChain'
-
-import {Lender} from "../contracts/Lender.Sol"
